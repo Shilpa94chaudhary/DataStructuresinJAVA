073b8ecff6e1f615236266b306800b697263a1be
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-09-20 22:15:23 +0530

    OOPS: Polynomial add, subtract, multiply

--- src/PolynomialClass/Polynomials.java
+++ src/PolynomialClass/Polynomials.java
@@ -4,22 +4,25 @@ public class Polynomials {
 	
 	private int polynomial[];
 	private int highestDegree;
-
+	
 	public Polynomials() {
-		polynomial = new int[1];
-		highestDegree = 0;
+		polynomial = new int[5];
+		highestDegree = -1;
 	}
 	
+	// This function will set the coefficient in a polynomial
 	public void setCoefficient(int degree, int coefficient) {
-		if(degree>highestDegree) {
-			createPolynomial(degree+1);
-			highestDegree = degree;
+		if(degree >= polynomial.length-1) {
+			increaseSize(degree+1);
 		}
 		polynomial[degree] = coefficient;
-		
+		if(highestDegree < degree) {
+			highestDegree = degree;
+		}
 	}
-
-	private void createPolynomial(int size) {
+	
+	// This function will increase the size of the 
+	private void increaseSize(int size) {
 		int temp[] = polynomial;
 		polynomial = new int[size];
 		for(int i = 0 ; i<temp.length ; i++) {
@@ -27,39 +30,82 @@ public class Polynomials {
 		}
 	}
 
+	// This function will add two polynomials and return the result
+	public Polynomials add(Polynomials second) {
+		
+		Polynomials result = new Polynomials();
+		
+		int i = 0, j = 0, k = 0;
+		
+		while( i<this.polynomial.length && j<second.polynomial.length) {
+			result.setCoefficient(k, this.polynomial[i]+second.polynomial[j]);
+			i++; j++; k++;
+		}
+		
+		while(i<this.polynomial.length) {
+			result.setCoefficient(k, this.polynomial[i]);
+			i++; k++;
+		}
+		
+		while(j<second.polynomial.length) {
+			result.setCoefficient(k,second.polynomial[j]);
+			j++; k++;
+		}
+		return result;
+	}
+
+	// This function will print the polynomial
 	public void print() {
-		for(int i = 0 ; i < polynomial.length ; i++) {
-			if(polynomial[i]==0) {
-				continue;
-			}else {
-				System.out.print(polynomial[i] + "x^" + i + " + ");
+		for(int i=0; i<polynomial.length ; i++) {
+			if(polynomial[i] != 0) {
+				System.out.print(polynomial[i] + "x" + i + " ");
 			}
 		}
-		System.out.println();
-	}
-
-	public void add(Polynomials p2) {
-		if(this.highestDegree < p2.highestDegree) {
-			int size = p2.highestDegree+1;
-			createPolynomial(size);
-			highestDegree = size;
-		}
-		for(int i = 0 ; i <= p2.highestDegree ; i++) {
-			this.polynomial[i] += p2.polynomial[i];
-		}
-	}
-
-	public void multiply(Polynomials p1) {
-		if(this.highestDegree == 0 && p1.highestDegree == 0) {
-			this.polynomial[0] = this.polynomial[0]*p1.polynomial[0];
-		}else if(this.highestDegree == 0){
-			
-		}
-	}
-
-	public static Polynomials add(Polynomials p1, Polynomials p2) {
 		
-		return null;
 	}
 
+	// This function will subtract second polynomial from first polynomial function called with
+	public Polynomials subtract(Polynomials second) {
+		
+		Polynomials result = new Polynomials();
+		
+		int i = 0, j = 0, k = 0;
+		
+		while( i<this.polynomial.length && j<second.polynomial.length) {
+			result.setCoefficient(k, this.polynomial[i]-second.polynomial[j]);
+			i++; j++; k++;
+		}
+		
+		while(i<this.polynomial.length) {
+			result.setCoefficient(k, this.polynomial[i]);
+			i++; k++;
+		}
+		
+		while(j<second.polynomial.length) {
+			result.setCoefficient(k, - second.polynomial[j]);
+			j++; k++;
+		}
+		return result;
+	}
+
+	// This function will multiply the second polynomial to first polynomial
+	/*
+	 * (2x2 + 3x4) * (2x0 + 2x3 + 2x4)
+	 * = 4x2 + 6x4 + 4x5 + 6x7 + 4x6 + 6x8
+	 */
+	public Polynomials multiply(Polynomials second) {
+		Polynomials result = new Polynomials();
+		
+		int degree = highestDegree + second.highestDegree;
+		result.setCoefficient(degree,0);
+		
+		for(int i = 0 ; i <= highestDegree ; i++) {
+			for(int j = 0 ; j <= second.highestDegree ; j++) {
+//				System.out.println("result.polynomial[i+j]"+ result.polynomial[i+j] + "polynomial[i]" + polynomial[i] + "second.polynomial[j]" + second.polynomial[j]);
+				result.setCoefficient(i+j , result.polynomial[i+j] + polynomial[i]*second.polynomial[j]);
+//				System.out.println("final value at: " + result.polynomial[i+j]);
+			}
+		}
+		return result;
+	}
 }
--- src/PolynomialClass/Polynomialuse.java
+++ src/PolynomialClass/Polynomialuse.java
@@ -1,34 +1,82 @@
 package PolynomialClass;
 
-public class Polynomialuse {
+import java.util.Scanner;
 
+public class Polynomialuse {
+	
 	public static void main(String[] args) {
 		
-		Polynomials p1 = new Polynomials();
-		p1.setCoefficient(0,3);
-		p1.setCoefficient(2,2);
-		p1.print();
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
 		
-		Polynomials p2 = new Polynomials();
-		p2.setCoefficient(0,3);
-		p2.setCoefficient(1,4);
-		p2.setCoefficient(2,5);
-		p2.setCoefficient(3,2);
-		p2.print();
+		System.out.print("Count of the number of coefficients in first polynomial: ");
+		int n = s.nextInt();
+		int degree1[] = new int[n];
 		
-		p1.add(p2);
-		p1.print();
-		p2.print();
+		System.out.print("Enter degree in first polynomial: ");
+		for(int i = 0; i < n; i++){
+			degree1[i] = s.nextInt();
+		}
 		
-		p2.multiply(p1);
-		p2.print();
-		p1.print();
+		System.out.print("Enter coefficients in first polynomial: ");
+		int coeff1[] = new int[n];
+		for(int i = 0; i < n; i++){
+			coeff1[i] = s.nextInt();
+		}
 		
-//		Polynomials p3 = Polynomials.add(p1,p2);
-//		p1.print();
-//		p2.print();
-//		p3.print();
+		Polynomials first = new Polynomials();
+		for(int i = 0; i < n; i++){
+			first.setCoefficient(degree1[i],coeff1[i]);
+		}
 		
+		System.out.print("Count of the number of coefficients in second polynomial: ");
+		n = s.nextInt();
+		
+		System.out.print("Enter degree in second polynomial: ");
+		int degree2[] = new int[n];
+		for(int i = 0; i < n; i++){
+			degree2[i] = s.nextInt();
+		}
+		
+		System.out.print("Enter coefficients in second polynomial: ");
+		int coeff2[] = new int[n];
+		for(int i = 0; i < n; i++){
+			coeff2[i] = s.nextInt();
+		}
+		
+		Polynomials second = new Polynomials();
+		for(int i = 0; i < n; i++){
+			second.setCoefficient(degree2[i],coeff2[i]);
+		}
+		
+		System.out.print("Enter choice : ");
+		int choice = s.nextInt();
+		Polynomials result;
+		switch(choice){
+		
+		// Add
+		case 1: 
+			 result = first.add(second);
+			result.print();
+			break;
+			
+		// Subtract	
+		case 2 :
+			 result = first.subtract(second);
+			result.print();
+			break;
+			
+		// Multiply
+			/*
+			 * (2x2 + 3x4) * (2x0 + 2x3 + 2x4)
+			 * = 4x2 + 6x4 + 4x5 + 6x7 + 4x6 + 6x8
+			 */
+		case 3 :
+			 result = first.multiply(second);
+			result.print();
+			break;
+		}
+
 	}
 
 }
