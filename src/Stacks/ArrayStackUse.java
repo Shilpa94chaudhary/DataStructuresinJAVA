58f163ef3207ffa2ee561235490aaa9ee59faace
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-10-26 10:56:59 +0530

    Stack

--- src/Stacks/ArrayStackUse.java
+++ src/Stacks/ArrayStackUse.java
@@ -5,58 +5,101 @@ public class ArrayStackUse {
 	public static void main(String[] args) {
 
 		StackUsingArray stack = new StackUsingArray();
+		
 		try {
 			stack.print();
 		} catch (StackUnderflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Underflow Exception");
 		}
+		
+		
 		try {
 			stack.push(10);
 		} catch (StackOverflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Overflow Exceptionn");
 		}
+		
+		
 		try {
 			stack.push(20);
 		} catch (StackOverflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Overflow Exceptionn");
 		}
+		
+		
 		try {
 			stack.push(30);
 		} catch (StackOverflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Overflow Exceptionn");
 		}
+		
+		
 		try {
 			stack.print();
 		} catch (StackUnderflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Underflow Exception");
 		}
-		System.out.println("Pop " + stack.pop());
+		
+		
+//		try {
+//			System.out.println("Pop " + stack.pop());
+//		} catch (StackUnderflowException e1) {
+//			System.out.println("Stack UnderflowE xception");
+//		}
+		
+		
 		try {
 			stack.print();
 		} catch (StackUnderflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Underflow Exception");
 		}
+		
+		
 		System.out.println("Size " + stack.size());
+		
+		
 		try {
 			System.out.println("Stack is empty " + stack.isEmpty());
 		} catch (StackUnderflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Underflow Exception");
 		}
-		
+
 		// Fill complete stack
 		try {
 			for(int i = 0; i<10 ; i++) {
 				stack.push(i*10);
 			}
 		} catch (StackOverflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Overflow Exceptionn");
 		}
+
+		System.out.println("Size " + stack.size());
+		
 		
 		// Print stack
 		try {
 			stack.print();
 		} catch (StackUnderflowException e) {
-			e.printStackTrace();
+			System.out.println("Stack Underflow Exception");
+		}
+
+		// Empty complete stack
+
+		for(int i = 0; i<10 ; i++) {
+			try {
+				System.out.println("Pop " + stack.pop());
+			} catch (StackUnderflowException e) {
+				System.out.println("Stack Underflow Exception");
+			}
+		}
+
+		System.out.println("Size " + stack.size());
+		
+		// Print stack
+		try {
+			stack.print();
+		} catch (StackUnderflowException e) {
+			System.out.println("Stack Underflow Exception");
 		}
 	}
 }
--- src/Stacks/StackUsingArray.java
+++ src/Stacks/StackUsingArray.java
@@ -10,10 +10,12 @@ public class StackUsingArray {
 		topIndex = -1;
 	}
 
+	// O(1)
 	public int size() {
 		return topIndex+1;
 	}
 
+	// O(1)
 	public boolean isEmpty() throws StackUnderflowException {
 		if(topIndex == -1) {
 			throw new StackUnderflowException();
@@ -21,21 +23,36 @@ public class StackUsingArray {
 		return false;
 	}
 
+	// O(1)
 	public void push(int element) throws StackOverflowException {
-		if(topIndex == 9) {
-			throw new StackOverflowException();
+		if(topIndex == data.length - 1) {
+//			throw new StackOverflowException();
+			doubleCapacity();
 		}
 		topIndex++;
 		data[topIndex] = element;
 	}
 
-	public int pop() {
+	private void doubleCapacity() {
+		int temp[] = data;
+		data = new int [temp.length * 2];
+		for(int i = 0; i<temp.length ; i++) {
+			data[i] = temp[i];
+		}
+	}
+
+	// O(1)
+	public int pop() throws StackUnderflowException {
+		if(topIndex == -1) {
+			throw new StackUnderflowException();
+		}
 		int temp = data[topIndex];
 		data[topIndex] = 0;
 		--topIndex;
 		return temp;
 	}
 
+	// O(1)
 	public int top() throws StackUnderflowException {
 		if(topIndex == -1) {
 			throw new StackUnderflowException();
@@ -43,6 +60,7 @@ public class StackUsingArray {
 		return data[topIndex];
 	}
 
+	// O(n)
 	public void print() throws StackUnderflowException {
 		if(topIndex == -1) {
 			throw new StackUnderflowException();
